82ec3c8930993982775a47ed1cb325f60ee51b5c
PabloPagánBeltrá <pablo23clase@gmail.com>
2020-02-03 11:22:28 +0100

    Versión Refactorizada

--- geometria/src/Circulo_PPB.java
+++ geometria/src/Circulo_PPB.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_PPB extends FiguraGeometrica_PPB {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_PPB(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
